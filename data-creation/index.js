67c4b78a1bed19f6cbce140ac042fdb67745808d
Arpit Bansal <arpit.bansal@outlook.com>
2018-05-10 10:49:01 +0530

    1. Connection to MongoDB Atlas 2. Data creation model

--- data-creation/index.js
+++ data-creation/index.js
@@ -6,11 +6,14 @@ var Category = require('./category-model');
 const fs = require ('fs');
 let catData = fs.readFileSync('categoriesLoad.json');
 let catJsonData = JSON.parse(catData);
+let credentialData = JSON.parse(fs.readFileSync('../config/config.json'));
 
-var connStr = 'mongodb://localhost:27017/test';
+var connStr = credentialData.dbStringPrefix+credentialData.username+":"+credentialData.password+credentialData.dbDetails+credentialData.environmentDetails.contentDb+credentialData.dbStringSuffix;
+
 mongoose.connect(connStr, function(err) {
     if (err) throw err;
     // console.log('Successfully connected to MongoDB');
+    console.log("connection is successful");
     _.each(catJsonData,function(v){
       // console.log("output: before saving"+JSON.stringify(v));
       var saveCat = new Category(v);
